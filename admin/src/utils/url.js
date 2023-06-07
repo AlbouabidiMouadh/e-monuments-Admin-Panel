8af1751ef4136bb15140e3bcaa709d8355db0488
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-06-08 00:44:53 +0100

    tables & modify guide

--- admin/src/utils/url.js
+++ admin/src/utils/url.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-const url  = "http://192.168.1.199:5000";
-=======
-const url  = "http://10.204.170.11:5000";
->>>>>>> 358102dc12681b3c216befd198b487a3cdd0d285
+// <<<<<<< HEAD
+const url  = "http://192.168.1.77:5000";
+// =======
+// const url  = "http://10.204.170.11:5000";
+// >>>>>>> 358102dc12681b3c216befd198b487a3cdd0d285
 export default url ;
